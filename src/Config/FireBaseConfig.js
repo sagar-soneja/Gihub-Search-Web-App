3bac0d52e1cd42d364ee17030954b5e4ae111f79
Sagar Soneja <78483562+sagar-soneja@users.noreply.github.com>
2021-07-13 00:19:19 +0530

    Delete src directory

--- src/Config/FireBaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyCAzOKXdsUzoffVBm8mj0MxRBTyWI1md2k",
-    authDomain: "mygitapp-2c205.firebaseapp.com",
-    projectId: "mygitapp-2c205",
-    storageBucket: "mygitapp-2c205.appspot.com",
-    messagingSenderId: "774882210295",
-    appId: "1:774882210295:web:5306e41cad07d32820bef2",
-    measurementId: "G-6KFC50GWTD"
-  };
